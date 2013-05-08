20441e2b397d0021e660f788ec20936d7c28ade1
Tom Igoe <tom.igoe@gmail.com>
2013-05-07 21:06:08 -0400

    Added SerialNumericParser example

---
+++ SerialNumericParser/SerialNumericParser.ino
@@ -0,0 +1,46 @@
+/*
+  Serial input with parsing
+ 
+ This example shows how to read a serial stream of 
+ comma-separated values, parse the ASCII numeric strings
+ int numeric variables, and use them.
+ 
+ The circuit:
+ * LEDs on pins 5 and 6. But could be used for many things.
+ 
+ created 7 May 2013
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Assumes a string in from the serial port like so:
+  // s ledNumber, brightness \n
+  // for example: "s5,200\n":
+
+  int ledNumber = 0;
+  int brightness = 0;
+  
+  if (Serial.find("s")) {
+    ledNumber = Serial.parseInt(); // parses numeric characters before the comma
+    brightness = Serial.parseInt();// parses numeric characters after the comma
+    
+    // print the results back to the sender:
+    Serial.print("LED number: " );
+    Serial.print(ledNumber);
+    Serial.print(" at ");
+    Serial.println(brightness);
+
+    // set the LED:
+    analogWrite(LED, brightness);
+  }
+}
+
+
+
