6bfe09f403d6a57284e15f74f77727bdc8a242f9
tigoe <tom.igoe@gmail.com>
2018-02-24 17:51:55 -0500

    updated sleeping logger

--- RTCSleepingLogger/RTCSleepingLogger.ino
+++ RTCSleepingLogger/RTCSleepingLogger.ino
@@ -7,73 +7,89 @@
   Note that the USB serial port will disappear while the board is in standby mode.
   To bring the board out of standby mode, double-tap the reset button.
 
-  RTC code based on Arturo Guadalupi's RTC sleep example 
-  created by Tom Igoe
-  20 Feb 2018
+  Serial printing happens only if the serial port is open on your computer
+  (that's what if (Serial) does).
+  
+  RTC code based on Arturo Guadalupi's RTC sleep example
+  created 20 Feb 2018
+  updated 24 Feb 2018
+  by Tom Igoe
 */
 
 #include <RTCZero.h>
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = 4;   // CS pin for the SD card. For MKRZero use SDCARD_SS_PIN
+const int chipSelect = SDCARD_SS_PIN;   // CS pin for the SD card. For MKRZero use SDCARD_SS_PIN
+volatile bool logging = false;          // flag to tell main loop to log data
 RTCZero rtc;                // realtime clock
 int state = LOW;            // LED state
 bool cardPresent = false;   // if the card is present
-float refVoltage = 0;       // reference voltage of the board
 
 void setup() {
+  Serial.begin(9600);
+  analogReference(AR_DEFAULT);
   Serial.println(__DATE__);         // print date of compile
   Serial.println(__TIME__);         // print time of compile
   pinMode(LED_BUILTIN, OUTPUT);     // set builtin LED to output
-  cardPresent = initializeCard();   // check to see if SD card is accessible
-
+  digitalWrite(LED_BUILTIN, HIGH);  // set it high
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    if (Serial) Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  cardPresent = true;
+  if (Serial) Serial.println("card initialized.");
+  
   rtc.begin();                      // start the realtime clock
-  rtc.setAlarmTime(00, 00, 00);     // set an alarm for processor wakeup
-  rtc.enableAlarm(rtc.MATCH_SS);    // enable it
-  rtc.attachInterrupt(alarmMatch);  // have the wakeup happen when the RTC seconds matches 00
+  rtc.setAlarmSeconds(00);          // set an alarm for processor wakeup
+  rtc.enableAlarm(rtc.MATCH_MMSS);  // enable it for once an hour
+  rtc.attachInterrupt(alarmMatch);  // have the wakeup happen on the alarm
+}
+
+void loop() {
+  if (logging) {                          // if you need to log,
+    String timestamp = getTimeStamp();    // get the timestamp
+    float tempNow = temperatureRead();    // get the temperature now
+    if (cardPresent) {                    // if the SD card is present
+      writeToCard(tempNow, timestamp);    // write temp and time to SD card
+    }
+    logging = false;                      // clear the logging flag
+    rtc.enableAlarm(rtc.MATCH_SS);        // enable alarms again
+  }
+
+  // if not logging, go to sleep on the 10th second of the minute:
+  if (rtc.getSeconds() == 10 && !logging) {
+    if (Serial) Serial.println("going into standby");
+    rtc.standbyMode();
+  }
 
   // to set the date and time (epoch, or seconds since 1/1/1970) from a POSIX machine, type:
   // $ date +D%DT%s
   // thanks to Paul Stoffregen for this trick
-
-  // wait ten seconds for user to input datetime via serial:
-  while (millis() < 10000) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    if (Serial.available()) {
-      // if you get the D
-      if (Serial.findUntil("D", "T")) {
-        rtc.setMonth(Serial.parseInt());  // set the month, day, year
-        rtc.setDay(Serial.parseInt());
-        rtc.setYear(Serial.parseInt());
-      } // if you get the T:
-      if (Serial.findUntil("T", "\n")) {
-        unsigned long epoch = Serial.parseInt();  // set the epoch
-        rtc.setEpoch(epoch);
-        Serial.println(getTimeStamp());   // print the timestamp
-      }
+  
+  while (Serial.available()) {
+    digitalWrite(LED_BUILTIN, HIGH);    // turn on LED to signal time reading
+    // if you get the D
+    if (Serial.findUntil("D", "T")) {
+      rtc.setMonth(Serial.parseInt());  // set the month, day, year
+      rtc.setDay(Serial.parseInt());
+      rtc.setYear(Serial.parseInt());
+    } // if you get the T:
+    if (Serial.findUntil("T", "\n")) {
+      unsigned long epoch = Serial.parseInt();  // set the epoch
+      rtc.setEpoch(epoch);
+      Serial.println(getTimeStamp());   // when done, print the timestamp
     }
   }
-  // serial delay over, turn off the LED:
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void loop() {
-  rtc.standbyMode();    // Sleep until next alarm match
+  digitalWrite(LED_BUILTIN, LOW);       // turn off LED to signal end of time reading
 }
 
 void alarmMatch() {
-  String timestamp = getTimeStamp();    // get the timestamp
-  float tempNow = temperatureRead();    // get the temperature now
-  digitalWrite(LED_BUILTIN, HIGH);      // turn on the LED
-  if (cardPresent) {                    // if the SD card is present
-    writeToCard(tempNow, timestamp);    // write temp and time to SD card
-  } else {                              // if card's not there,
-    Serial.print(timestamp);            // print data serially
-    Serial.print(",");
-    Serial.println(tempNow);
-  }
-  digitalWrite(LED_BUILTIN, LOW);       // turn off the LED after writing
+  logging = true;       // set the logging flag so loop knows to log
+  rtc.disableAlarm();   // disable alarms
 }
 
 String getTimeStamp() {
@@ -102,53 +118,44 @@ String getTimeStamp() {
 
 float temperatureRead() {  // read the temperature sensor:
   int tempSensor = analogRead(A0);
-  delay(1);   // let the ADC stabilize
-  // read the board voltage on A1:
-  refVoltage = (analogRead(A1) / 1023.0) * 3.3;
   // convert temp to voltage:
-  float voltage = (tempSensor / 1023.0) * refVoltage;
+  float voltage = (tempSensor / 1023.0) * 3.3;
   // convert to temperature (degrees Celsius):
   float temperature = (voltage - 0.5) / 0.01;
   return temperature;
-}
-
-bool initializeCard() {
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(10, OUTPUT);
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return false;
-  }
-  Serial.println("card initialized.");
-  return true;
 }
 
 
 bool writeToCard(float temp, String datetime) {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  // this function will make a new file every day
+  // filename is 8.3: 8chars, 3 chars in extension max.
+  String fileName = "20";
+  fileName += rtc.getYear();
+  fileName += rtc.getDay();
+  fileName += rtc.getMonth();
+  fileName += ".txt";
+  File dataFile = SD.open(fileName, FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
+    if (Serial) Serial.println(datetime);
+    digitalWrite(LED_BUILTIN, HIGH);      // turn on LED to signal that you're writing
     dataFile.print(datetime);
-    dataFile.print(",");
-    dataFile.print(refVoltage);
-    dataFile.print("V,");
+    dataFile.print(", ");
     dataFile.print(temp);
-    dataFile.println("deg.C,");
+    dataFile.println(" deg.C");
     dataFile.close();
-    Serial.println("data written");
+    digitalWrite(LED_BUILTIN, LOW);      // turn off LED after writing
     return true;
   }
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening  datalog.txt");
+    if (Serial) {
+      Serial.print("error opening  ");
+      Serial.println(fileName);
+    }
     return false;
   }
 }
