284e1a9bc0c04712246d5babc263288f1a4de49d
tigoe <tom.igoe@gmail.com>
2018-12-07 07:37:36 -0500

    update emic example, add another emic example

--- EmicSoftwareSerial/EmicSoftwareSerial.ino
+++ EmicSoftwareSerial/EmicSoftwareSerial.ino
@@ -1,27 +1,27 @@
 /*
-  EmicAnalogReadSerial
+  EmicSoftwareSerial
 
   Reads an analog input on pin 0 and reads the serial output
-  from the Serial1 TX pin using an Emic 2 Text-to-speech module.
+  from the SoftwareSerial TX pin using an Emic 2 Text-to-speech module.
   (https://www.sparkfun.com/products/11711)
   This is the minimal example for the Emic module.
 
-  Note: Serial1 is only available on the MKR boards, the Leonardo, the
-  Micro, and the Due. For Uno boards, use SoftwareSerial instead.
-
   Attach the center pin of a potentiometer to pin A0,
   and the outside pins to +5V and ground. Attach the SIN pin of the
-  Emic2 to the TX pin of the Arduino
+  Emic2 to pin 3 pin of the Arduino
 
 */
+#include <SoftwareSerial.h>
+
+SoftwareSerial emic(2,3);  // RX = 2, TX = 3
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  Serial1.begin(9600);
+  emic.begin(9600);
   // set the word rate at 400 words per minute (can go 75 - 600 wpm):
-  Serial1.println("W400");
+  emic.println("W400");
 }
 
 // the loop routine runs over and over again forever:
@@ -30,6 +30,6 @@ void loop() {
   int sensorValue = analogRead(A0);
   // print out the value you read:
   Serial.println("S" + String(sensorValue));
-  Serial1.println("S" + String(sensorValue));
+  emic.println("S" + String(sensorValue));
   delay(500);        // delay in between reads for voice legibility
 }
