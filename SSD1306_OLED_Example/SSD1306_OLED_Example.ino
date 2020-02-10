2623a43b5364a1ab346fe8cef38bcd6022e18a33
tigoe <tom.igoe@gmail.com>
2020-02-10 12:27:40 -0500

    Update SSD1306_OLED_Example.ino

--- SSD1306_OLED_Example/SSD1306_OLED_Example.ino
+++ SSD1306_OLED_Example/SSD1306_OLED_Example.ino
@@ -1,3 +1,23 @@
+/*
+  Minimal example for SSD1306 OLED display
+  manufacturer's site: http://www.solomon-systech.com/en/product/advanced-display/oled-display-driver-ic/ssd1306/
+  datasheet: https://www.crystalfontz.com/controllers/SolomonSystech/SSD1306/339/
+  library site: https://github.com/adafruit/Adafruit_SSD1306
+    
+  These displays are common and inexpensive online, and require only the
+  I2C pins (SDA and SCL) from an Arduino. The Adafruit library works well with them.
+  If you want custom fonts, check out fonts from http://oleddisplay.squix.ch/#/home
+
+  This example reads the millis() and ana analog sensor and displays both.
+  It's written for a 128x64 display, but can be modified to fit a 128x32 display as well.
+  Change the SCREEN_HEIGHT to change displays, and adjust font sizes and cursor locations
+  to suit your application.
+
+  created 10 Feb 2020
+  by Tom Igoe
+  based on the Adafruit_SSD1306 library examples
+ */
+ 
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -8,9 +28,11 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void setup() {
+  // initialize serial and wait for serial monitor to open:
   Serial.begin(9600);
   while (!Serial);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  // I2C address is 0x3C
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println("Display setup failed");
     while (true);
   }
