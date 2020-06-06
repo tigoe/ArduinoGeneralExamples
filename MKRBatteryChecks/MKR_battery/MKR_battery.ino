faf4b44380cef92e46726b3ce770cb67b392b735
tigoe <tom.igoe@gmail.com>
2020-06-06 09:10:53 -0400

    Update MKR_battery.ino

--- MKRBatteryChecks/MKR_battery/MKR_battery.ino
+++ MKRBatteryChecks/MKR_battery/MKR_battery.ino
@@ -1,10 +1,15 @@
+/*
+ ADC_BATTERY is pin 33 for the MKR boards, though it's only 
+ declared in the MKRZero board definition.
+ */
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   // read the battery:
-  int adcBatt = analogRead(ADC_BATTERY);
+int adcBatt = analogRead(ADC_BATTERY);
   // convert to a voltage:
   float vBatt = adcBatt * (4.3 / 1023.0);
   float percentage = (vBatt / 4.3) * 100;
