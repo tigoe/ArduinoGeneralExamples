5d826d07f479bfdc50f8f8cbaaa50a3c76fbae7c
Tom Igoe <tom.igoe@gmail.com>
2020-11-01 15:34:45 -0500

    Update 8x8LEDMatrix.ino

--- LED_Matrices/8x8-LED-Matrix/8x8LEDMatrix/8x8LEDMatrix.ino
+++ LED_Matrices/8x8-LED-Matrix/8x8LEDMatrix/8x8LEDMatrix.ino
@@ -1,25 +1,29 @@
-//int col[] = {2, 9, A7, 5, A3, A6, A2, A4};
-//int row[] = {8, A1, A0, 3, A5, 4, 7, 6};
+
 int col[] = {6, A2, A3, 3, A5, 4, 8, 9};
 int row[] = {2, 7, A7, 5, A0, A6, A1, A4};
 
 void setup() {
-  // put your setup code here, to run once:
+  // initialize all the pins as outputs:
   for (int i = 0; i < 8; i++) {
     pinMode(row[i], OUTPUT);
     pinMode(col[i], OUTPUT);
-    digitalWrite(row[i], HIGH);
+    // set the column pins HIGH
+    digitalWrite(col[i], HIGH);
   }
 }
 
 void loop() {
-  for (int c = 0; c < 8; c++) {
-    digitalWrite(col[c], HIGH);
-    for (int r = 0; r < 8; r++) {
-      digitalWrite(row[r], LOW);
+  // take the rows high:
+  for (int r = 0; r < 8; r++) {
+    digitalWrite(row[r], HIGH);
+    // Take each column low to turn on (row, column):
+    for (int c = 0; c < 8; c++) {
+      digitalWrite(col[c], LOW);
       delay(100);
-      digitalWrite(row[r], HIGH);
+      // take the col high to turn off the LED:
+      digitalWrite(col[c], HIGH);
     }
-    digitalWrite(col[c], LOW);
+    // take the row low to turn off the whole row:
+    digitalWrite(row[r], LOW);
   }
 }
